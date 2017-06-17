500451c1beab2d3ea8cd050ed25b100060801562
Shruti Ramesh <shramesh@Shrutis-MacBook-Air.local>
2017-06-17 15:25:46 -0700

    Trying to run steps in parallel

--- parallel-jobs/parallelLocations.groovy
+++ parallel-jobs/parallelLocations.groovy
@@ -5,36 +5,25 @@ node('all') {
     wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
         String locations_str = "${LOCATIONS}"
         def locations = locations_str.tokenize('[ \t\n]+')
+        def executions = [:]
 
-        stage("DeployToLocations") {
-            for (i=0; i < locations.size(); i++) {
-                env.LOCATION = locations[i]
-                stage("${LOCATION}") {
-                    // in this array we'll place the jobs that we wish to run
-                    def branches = [:]
-
-                    //running the job 4 times concurrently
-                    //the dummy parameter is for preventing mutation of the parameter before the execution of the closure.
-                    //we have to assign it outside the closure or it will run the job multiple times with the same parameter "4"
-                    //and jenkins will unite them into a single run of the job
-
-                    for (int i = 0; i < 4; i++) {
-                        def index = i //if we tried to use i below, it would equal 4 in each job execution.
-                        branches["branch${i}"] = {
-                        //Parameters:
-                        //param1 : an example string parameter for the triggered job.
-                        //dummy: a parameter used to prevent triggering the job with the same parameters value. this parameter has to accept a different value
-                        //each time the job is triggered.
-                            build job: 'test_jobs', parameters: [[$class: 'StringParameterValue', name: 'param1', value:
-                            'test_param'], [$class: 'StringParameterValue', name:'dummy', value: "${index}"]]
-                            }
+        def create_execution(String dist) {
+            stage("${dist}") {
+                cmd = { 
+                    node {
+                        sh "echo ${dist}"
                     }
-                    parallel branches
-
                 }
+                return cmd
             }
-        }    
-    }             
+        }
+
+        for(int i = 0; i < locations.size(); i++) {
+            executions[locations[i]] = create_execution(locations[i])
+            }
+
+        parallel executions
+    }       
 }    
 
 
