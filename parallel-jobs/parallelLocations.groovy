154a1e620075bea7a695e7563422d536495eeba5
Shruti Ramesh <shramesh@Shrutis-MacBook-Air.local>
2017-06-17 15:30:40 -0700

    Trying to run steps in parallel

--- parallel-jobs/parallelLocations.groovy
+++ parallel-jobs/parallelLocations.groovy
@@ -5,21 +5,37 @@ node('all') {
     wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
         String locations_str = "${LOCATIONS}"
         def locations = locations_str.tokenize('[ \t\n]+')
-        def executions = [:]
+        
+        def stringsToEcho = ["a", "b", "c", "d"]
 
-        for(int i = 0; i < locations.size(); i++) {
-            executions[locations[i]] = create_execution(locations[i])
-            }
+        // The map we'll store the parallel steps in before executing them.
+        def stepsForParallel = [:]
 
-        parallel executions
-        def create_execution(String dist) {
-            stage("${dist}") {
-                cmd = { 
-                    node {
-                        sh "echo ${dist}"
-                    }
+        for (int i = 0; i < stringsToEcho.size(); i++) {
+            def s = stringsToEcho.get(i)
+
+            // Transform that into a step and add the step to the map as the value, with
+            // a name for the parallel step as the key. Here, we'll just use something
+            // like "echoing (string)"
+            def stepName = "echoing ${s}"
+            
+            stepsForParallel[stepName] = transformIntoStep(s)
+        }
+
+        // Actually run the steps in parallel - parallel takes a map as an argument,
+        // hence the above.
+        parallel stepsForParallel
+
+        // Take the string and echo it.
+        def transformIntoStep(inputString) {
+            // We need to wrap what we return in a Groovy closure, or else it's invoked
+            // when this method is called, not when we pass it to parallel.
+            // To do this, you need to wrap the code below in { }, and either return
+            // that explicitly, or use { -> } syntax.
+            return {
+                node {
+                    echo inputString
                 }
-                return cmd
             }
         }
     }       
