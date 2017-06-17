de5ff2154077bc8560d7b83b1d7ba01c8e05a06e
Shruti Ramesh <shramesh@Shrutis-MacBook-Air.local>
2017-06-17 15:35:14 -0700

    Trying to run steps in parallel

--- parallel-jobs/parallelLocations.groovy
+++ parallel-jobs/parallelLocations.groovy
@@ -11,14 +11,13 @@ node('all') {
         // The map we'll store the parallel steps in before executing them.
         def stepsForParallel = [:]
 
-        for (int i = 0; i < stringsToEcho.size(); i++) {
-            def s = stringsToEcho.get(i)
+        for (int i = 0; i < locations.size(); i++) {
+            def s = locations.get(i)
 
             // Transform that into a step and add the step to the map as the value, with
             // a name for the parallel step as the key. Here, we'll just use something
             // like "echoing (string)"
-            def stepName = "echoing ${s}"
-            
+            def stepName = "testing ${s}"
             stepsForParallel[stepName] = transformIntoStep(s)
         }
 
@@ -32,7 +31,9 @@ node('all') {
 def transformIntoStep(inputString) {
     return {
         node {
-            echo inputString
+            stage(${inputString}) {
+                echo inputString
+            }
         }
     }
 }
